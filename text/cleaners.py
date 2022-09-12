480b5d98d433949213ad1df55be4786dbc8ac5d1
tailong.shi <tailong.shi@advancegroup.com>
2022-09-12 12:23:16 +0800

    fix sokuons

--- text/cleaners.py
+++ text/cleaners.py
@@ -208,6 +208,27 @@ _bopomofo_to_romaji = [(re.compile('%s' % x[0], re.IGNORECASE), x[1]) for x in [
   ('—', '-')
 ]]
 
+# Map of (sokuon, voice) pairs:
+_sokuon_to_voice = {
+  'b': 'p',
+  'c': 't',
+  'd': 't',
+  'f': 'f',
+  'g': 'k',
+  'j': 't',
+  'k': 'k',
+  'l': 'l',
+  'm': 'm',
+  'n': 'n',
+  'p': 'p',
+  'q': 'k',
+  'r': 'r',
+  's': 's',
+  't': 't',
+  'v': 'f',
+  'z': 't'
+}
+
 
 def expand_abbreviations(text):
   for regex, replacement in _abbreviations:
@@ -283,21 +304,30 @@ def japanese_to_full_romaji_with_tone_letters(text):
         text += ' '
       labels = pyopenjtalk.extract_fullcontext(sentence)
       for n, label in enumerate(labels):
+        # p3
         phoneme = re.search(r'\-([^\+]*)\+', label).group(1)
-        if phoneme not in ['sil', 'pau']:
-          a3 = int(re.search(r"\+(\d+)/", label).group(1))
-          if a3 != 1:
-            phoneme_next = re.search(r'\-([^\+]*)\+', labels[n + 1]).group(1)
-            cl_replace = 'Q' if phoneme_next in 'aeiou' else phoneme_next
-          else:
-            cl_replace = '!'
-          text += phoneme.replace('ch', 'ʧ').replace('sh', 'ʃ')\
-            .replace('cl', cl_replace).replace('ts','ʦ')
-        else:
+        if phoneme in ['sil', 'pau']:
           continue
+
+        a3 = int(re.search(r"\+(\d+)/", label).group(1))
+        # p4
+        phoneme_next = re.search(r"\+([^=]*)=", label).group(1)
+        if a3 != 1:
+          cl_replace = real_voice_for_sokkuon(phoneme_next)
+        else:
+          cl_replace = 'Q'
+        text += phoneme.replace('ch', 'ʧ').replace('sh', 'ʃ').replace('ts', 'ʦ').replace('cl', cl_replace)
+
         a1 = int(re.search(r"/A:(\-?[0-9]+)\+", label).group(1))
         a2 = int(re.search(r"\+(\d+)\+", label).group(1))
+        if phoneme_next in ['sil', 'pau']:
+          a2_next = -1
+        else:
+          a2_next = int(re.search(r"\+(\d+)\+", labels[n + 1]).group(1))
 
+        # the same mora
+        if a2_next == a2:
+          continue
         # morae after the accent. L or ˧ for low.
         if a1 > 0:
           text += '˧'
@@ -313,6 +343,11 @@ def japanese_to_full_romaji_with_tone_letters(text):
     if i < len(marks):
       text += unidecode(marks[i]).replace(' ', '').replace('...','…')
   return text
+
+
+def real_voice_for_sokkuon(phoneme_next):
+  return _sokuon_to_voice.get(phoneme_next[0], 'Q')
+
 
 def latin_to_hangul(text):
   for regex, replacement in _latin_to_hangul:
@@ -338,7 +373,7 @@ def hangul_number(num, sino=True):
   digits = '123456789'
   names = '일이삼사오육칠팔구'
   digit2name = {d: n for d, n in zip(digits, names)}
-  
+
   modifiers = '한 두 세 네 다섯 여섯 일곱 여덟 아홉'
   decimals = '열 스물 서른 마흔 쉰 예순 일흔 여든 아흔'
   digit2mod = {d: mod for d, mod in zip(digits, modifiers.split())}
